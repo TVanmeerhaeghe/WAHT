99e292daf2ce53af7febced60ea7c4af44b12a13
TVanmeerhaeghe <vanmeerhaegheteo@gmail.com>
2026-03-19 17:39:36 +0100

    fix: add vue eslint parser for .vue files, fix nuxt prepare path

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,9 +1,20 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+
 export default [
   {
     ignores: ['node_modules/**', '.nuxt/**', '.output/**']
   },
   {
-    files: ['**/*.vue', '**/*.ts', '**/*.js'],
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser
+    },
+    plugins: { vue: pluginVue },
+    rules: {}
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
     rules: {}
   }
 ]
